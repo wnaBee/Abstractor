232bbec63383a2841444e207321673a1907824ae
Pec Muhn <79809244+wnaBee@users.noreply.github.com>
2021-04-11 15:00:46 +0200

    Add files via upload
    working on line splitter logic, figuring out how to use vectors, and how to make the logic as short as possible

--- filereader.cpp
+++ filereader.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+string commentor(string CodeLine);
+
 int main(int argc, char* argv[]){
 	fstream readfile;
 	fstream putfile;
@@ -13,21 +17,24 @@ int main(int argc, char* argv[]){
 		cout << "./filereader [file] [output file extension]";
 	}else{
 		readfile.open(argv[1], ios::in);
+		//define output file
 		for(int i = 0; i < Exlen; i++){
 			if(argv[2] == Extensions[i]){
 				output = "output" + Extensions[i];
 			}
 		}
 		if(readfile.is_open()){
+			//write to a new file
 			putfile.open(output, ios::out);
 			string line;
 			while(getline(readfile, line)){
 
 				 cout <<"oldlines "<< line << "\n";
-				 line = line + "ppp\n";
+				 line = line + commentor(line) + "\n";
 				 putfile << line;
 			}
 			putfile.close();
+			//check the new file
 			putfile.open(output, ios::in);
 			while(getline(putfile, line)){
 				cout <<"newlines "<< line << "\n";
@@ -39,3 +46,36 @@ int main(int argc, char* argv[]){
 return 0;
 }
 
+string commentor(string CodeLine){
+	int fulline = 0;
+	int lineSeg = 0;
+
+	vector <string> lineArray;
+/*	
+	while(fulline < Codeline.length){ //set current lineArray location
+		for(fulline < CodeLine.length(); fulline++){ //iterate through line
+			switch (CodeLine[fulline]){
+				case (isalnum(CodeLine[fulline])):
+					lineArray.operator[](fulline) += CodeLine[fulline];
+					break;
+				case (CodeLine[fulline] == "."):
+
+					break;
+				//below is a retarded way of handling operators. Can it be trimmed to `for(CodeLine[i] == (+||-||/||*||=||%||<||>||!||'|'||&||?||^||~||etc.))` or similar?
+				case (CodeLine[fulline] == "=" && CodeLine[fulline+1] != "=" || CodeLine[fulline] == "+" && CodeLine[fulline+1] != "=" || CodeLine[fulline] == "-" && CodeLine[fulline+1] != "=" || CodeLine[fulline] == "*" && CodeLine[fulline+1] != "="):
+				break;
+			}
+
+		}
+		lineArray.push_back();
+	}
+*/
+	string comment = "ppp";
+	return comment;
+}
+
+/*list of operators:
+ * ['+','-','/','*','%','<','>','&','|','!','?','^','~','=']
+ *list of deliniators:
+ *['.',',','(',')','[',']','{','}',':',';','\\'] 
+ */
