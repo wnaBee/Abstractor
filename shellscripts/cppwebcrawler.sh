ff03c990d59904132b0b1aee72cacb557431b19f
Pec Muhn <79809244+wnaBee@users.noreply.github.com>
2021-05-04 20:15:02 +0200

    Update cppwebcrawler.sh

--- shellscripts/cppwebcrawler.sh
+++ shellscripts/cppwebcrawler.sh
@@ -9,19 +9,17 @@
 #webcrawl
 #curl -s http://www.cplusplus.com/reference/ | grep -oP '(?<=<a href= ").*?(?=">)'
 
-#childs=$(curl -s http://www.cplusplus.com/reference/ | grep -oP '(?<=<a href= ").*?(?=">)')
+childs=$(curl -s http://www.cplusplus.com/reference/ | grep -oP '(?<=<a href= ").*?(?=">)')
 wait
 i=66
-#for url in $childs
-cat ./additionallinks.txt | while read url
+for url in $childs
 do
     ((i=i+1))
     echo $url
-   # curl -s http://www.cplusplus.com${url} | grep -v '<br>' | grep -oP '(?<=<b>).*?(?=</b>)' | sed 's/<.*>//gp' > ./cppg/cppc/cppcommands${i}.txt
+    wait
+    curl -s http://www.cplusplus.com${url} | grep -v '<br>' | grep -oP '(?<=<b>).*?(?=</b>)' | sed 's/<.*>//gp' > ./cppg/cppc/cppcommands${i}.txt
     wait
     curl -s http://www.cplusplus.com${url} | grep -v '<br>' | grep -oP '(?<=<dd>).*?(?=<span)' | sed 's/<.*>//gp'> ./cppg/cppd/cppdescriptions${i}.txt
     wait
-    #cat ./cppg/cppd/cppdescriptions${i}.txt
-    echo "_____________________________"
 done
 
