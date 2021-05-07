93e2e52edda7ece5d55d7fe7d6b3d76f8848cffa
Pec Muhn <79809244+wnaBee@users.noreply.github.com>
2021-05-07 16:12:31 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,7 +7,7 @@ if [[ -z ${compchk} ]]
 then
 	echo "g++ has not been installed, please install it by running: sudo apt-get install g++"
 else
-	sudo g++ filereader.cpp -o /usr/bin/filereader
+	sudo g++ src/filereader.cpp -o /usr/bin/filereader
 	echo "alias abstract='/usr/bin/filereader'" >> ~/.bash_aliases
 	source ~/.bash_aliases
 fi
