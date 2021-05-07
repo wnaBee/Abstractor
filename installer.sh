9384732b9f07ac37eb638936d2c9821f87f40969
Pec Muhn <79809244+wnaBee@users.noreply.github.com>
2021-05-07 21:23:58 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,7 +7,7 @@ if [[ -z ${compchk} ]]
 then
 	echo "g++ has not been installed, please install it by running: sudo apt-get install g++"
 else
-	sudo g++ filereader.cpp -o /usr/local/bin/filereader
+	sudo g++ src/filereader.cpp -o /usr/local/bin/filereader
 	cp src/formatter.sh /usr/local/bin/formatter.sh
 	echo "alias abstract='/usr/local/bin/formatter.sh'" >> ~/.bash_aliases
 	source ~/.bash_aliases
