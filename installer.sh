1a0f4bbfd7bc903fd08f20c96905d179c68eabb8
Pec Muhn <79809244+wnaBee@users.noreply.github.com>
2021-06-06 22:18:23 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -7,10 +7,9 @@ if [[ -z ${compchk} ]]
 then
 	echo "g++ has not been installed, please install it by running: sudo apt-get install g++"
 else
-	sudo g++ src/filereader.cpp -o /usr/local/bin/filereader
+	sudo g++ -I src/cppcode -I src/pythoncode -I src src/filereader.cpp -o /usr/local/bin/filereader
 	cp src/formatter.sh /usr/local/bin/formatter.sh
 	echo "alias abstract='/usr/local/bin/formatter.sh'" >> ~/.bash_aliases
-	sudo chmod a+x /usr/local/bin/formatter.sh
 	source ~/.bash_aliases
 fi
 
