d88efddb5429ed2dc850bf9338bae19324334435
Pec Muhn <79809244+wnaBee@users.noreply.github.com>
2021-06-28 11:43:15 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -8,8 +8,8 @@ then
 	echo "g++ has not been installed, please install it by running: sudo apt-get install g++"
 else
 	sudo g++ -I src/cppcode -I src/pythoncode -I src src/filereader.cpp -o /usr/local/bin/filereader
-	cp src/formatter.sh /usr/local/bin/formatter.sh
-	echo "alias abstract='/usr/local/bin/formatter.sh'" >> ~/.bash_aliases
+	cp src/formatter.sh /opt/formatter.sh
+	echo "alias abstract='/opt/formatter.sh'" >> ~/.bash_aliases
 	source ~/.bash_aliases
 fi
 
